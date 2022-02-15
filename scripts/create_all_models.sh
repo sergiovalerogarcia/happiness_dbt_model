1db39a7211d73022742ec065abb963f68608ae7c
Sergio Valero García <sergiovalero92@gmail.com>
2022-02-15 21:41:18 +0100

    Fix scripts/create_all_models.sh

--- scripts/create_all_models.sh
+++ scripts/create_all_models.sh
@@ -13,43 +13,43 @@ Run models...
 ./scripts/run_countries_dim.sh ""
 
 ./scripts/run_happiness_facts.sh \
-    "happiness.happiness.world_happiness_dataset_2015" \
+    "happiness.world_happiness_dataset_2015" \
     "2015_2016" \
     "2015" \
     "false" ""
 
 ./scripts/run_happiness_facts.sh \
-    "happiness.happiness.world_happiness_dataset_2016" \
+    "happiness.world_happiness_dataset_2016" \
     "2015_2016" \
     "2016" \
     "true" ""
 
 ./scripts/run_happiness_facts.sh \
-    "happiness.happiness.world_happiness_dataset_2017" \
+    "happiness.world_happiness_dataset_2017" \
     "2017" \
     "2017" \
     "true" ""
 
 ./scripts/run_happiness_facts.sh \
-    "happiness.happiness.world_happiness_dataset_2018" \
+    "happiness.world_happiness_dataset_2018" \
     "2018_2019" \
     "2018" \
     "true" ""
 
 ./scripts/run_happiness_facts.sh \
-    "happiness.happiness.world_happiness_dataset_2019" \
+    "happiness.world_happiness_dataset_2019" \
     "2018_2019" \
     "2019" \
     "true" ""
 
 ./scripts/run_happiness_facts.sh \
-    "happiness.happiness.world_happiness_dataset_2020" \
+    "happiness.world_happiness_dataset_2020" \
     "2020_2021" \
     "2020" \
     "true" ""
 
 ./scripts/run_happiness_facts.sh \
-    "happiness.happiness.world_happiness_dataset_2021" \
+    "happiness.world_happiness_dataset_2021" \
     "2020_2021" \
     "2021" \
     "true" ""
