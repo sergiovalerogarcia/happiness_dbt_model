6fa3864cace371e74c39c187ba6a20d2ca32c799
Sergio Valero García <sergiovalero92@gmail.com>
2022-02-21 11:37:49 +0100

    Add DBT_ to env vars

--- macros/if_table_exists_var_is_true_delete_from_where.sql
+++ macros/if_table_exists_var_is_true_delete_from_where.sql
@@ -1,6 +1,6 @@
-{% macro if_table_exists_var_is_true_delete_from_where(name, where_clause) %}
+{% macro if_table_exists_var_is_true_delete_from_where(name, schema, where_clause) %}
     {% if var('table_exists', 'true') == 'true' %}
-        delete from {{ env_var("DBNAME") }}.{{ env_var("SCHEMA") }}.{{ name }}
+        delete from {{ target.dbname }}.{{ schema }}.{{ name }}
         where {{ where_clause }}
     {% endif %}
 {% endmacro %}
--- models/happiness_facts/happiness_facts.sql
+++ models/happiness_facts/happiness_facts.sql
@@ -1,8 +1,9 @@
 {{
   config(
     pre_hook=if_table_exists_var_is_true_delete_from_where(
-      dbt_testing.generate_name_with_execution_type('happiness_facts'),
-      'year = ' ~ var('year', '2020') ~ '::int;'
+      name=dbt_testing.generate_name_with_execution_type('happiness_facts'),
+      schema=this.schema,
+      where_clause='year = ' ~ var('year', '2020') ~ '::int;'
     ),
     materialized='incremental',
     alias=dbt_testing.generate_name_with_execution_type(this.name)
